dc0808a8f19ad6dddb7da86392b820fb418544b8
Pirin <chuckdanorris123@gmail.com>
2022-09-17 02:40:02 -0400

    fixed typos on main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"github.com/piringraph/serverless-aws/pkg/handlers"
+
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"github.com/aws/aws-sdk-go/service/lambda"
-	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-var(
+var (
 	dynaClient dynamodbiface.DynamoDBAPI
 )
 
@@ -21,28 +21,28 @@ func main() {
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
-	},)
+	})
 
-	if err!=nil{
+	if err != nil {
 		return
 	}
 	dynaClient = dynamodb.New(awsSession)
 	lambda.Start(handler)
 }
 
-const tablename = "LambdaInGoUser"
+const tableName = "severless-aws"
 
-func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error){
-		switch req.HTTPMethod{
-		case "GET":
-			return handlers.GetUser(req, tableName, dynaClient)
-		case "POST":
-			return handlers.CreateUser(req, tableName, dynaClient)
-		case "PUT":
-			return handlers.UpdateUser(req, tableName, dynaClient)
-		case "DELETE":
-			return handlers.DeleteUser(req, tableName, dynaClient)
-		}
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(req, tableName, dynaClient)
+	case "DELETE":
+		return handlers.DeleteUser(req, tableName, dynaClient)
 	default:
-		return handlers.UnhandledMethod
+		return handlers.UnhandledMethod()
+	}
 }
