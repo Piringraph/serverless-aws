659cdbe934edab2755936e072d1f79142e5a85f9
Pirin <chuckdanorris123@gmail.com>
2022-09-17 02:20:21 -0400

    updated validators package routes

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 
-	"github.com/piringraph/serverless-aws/validators"
+	"github.com/piringraph/serverless-aws/pkg/validators"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
